6f7f4b54a63db7cca5f19281705eb461d428afcc
ponurakk <p0unrakg@int.pl>
2024-05-17 21:01:57 +0200

    feat: Serve docs at /docs route

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include "logger.h"
 #include <crow/app.h>
+#include <crow/common.h>
+#include <crow/http_response.h>
 #include <crow/logging.h>
 
 using crow::LogLevel;
@@ -10,6 +12,21 @@ int main() {
   crow::logger::setHandler(&logger);
   crow::SimpleApp app;
   app.loglevel(LogLevel::INFO);
+
+  // Default route to serve index.html from /static/docs
+  CROW_ROUTE(app, "/docs/")
+  ([](crow::response &res) {
+    res.redirect("/docs/index.html");
+    res.end();
+  });
+
+  CROW_ROUTE(app, "/docs/<path>")
+      .methods(crow::HTTPMethod::GET)(
+          [](const crow::request &req, crow::response &res, std::string path) {
+            std::string full_path = CROW_STATIC_DIRECTORY "docs/" + path;
+            res.set_static_file_info(full_path);
+            res.end();
+          });
 
   CROW_ROUTE(app, "/")
   ([]() {
