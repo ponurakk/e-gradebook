4b2e61f109aa5875903c80ff33b9668396471255
ponurakk <p0unrakg@int.pl>
2024-05-23 00:47:51 +0200

    refactor: Move docs serving to separate fn

--- src/main.cpp
+++ src/main.cpp
@@ -9,16 +9,7 @@
 using crow::LogLevel;
 using std::string;
 
-int main() {
-  CustomLogger logger;
-  crow::logger::setHandler(&logger);
-  crow::SimpleApp app;
-  app.loglevel(LogLevel::INFO);
-
-  Config config = {"127.0.0.1", 3000, "sqlite.db"};
-
-  DbInit db(config);
-
+void serveDocs(crow::SimpleApp &app) {
   // Default route to serve index.html from /static/docs
   CROW_ROUTE(app, "/docs/")
   ([](crow::response &res) {
@@ -33,6 +24,18 @@ int main() {
             res.set_static_file_info(full_path);
             res.end();
           });
+}
+
+int main() {
+  CustomLogger logger;
+  crow::logger::setHandler(&logger);
+  crow::SimpleApp app;
+  app.loglevel(LogLevel::INFO);
+
+  Config config = {"127.0.0.1", 3000, "sqlite.db"};
+
+  DbInit db(config);
+  serveDocs(app);
 
   CROW_ROUTE(app, "/")
   ([]() {
