4ef6f8a4a657f7d025946284ac4c076c86acdc4b
ponurakk <p0unrakg@int.pl>
2024-05-28 21:12:00 +0200

    feat: Add more functionality on frontend

--- static/main.js
+++ static/main.js
@@ -1,4 +1,4 @@
-const { dialog, div, h1, h2, button, form, input, p, label, nav, a, section, span } = van.tags;
+const { dialog, div, h1, h2, button, form, input, p, label, nav, a, section, span, select, option } = van.tags;
 
 const API_BASE_URL = '/api';
 const STUDENTS_URL = `${API_BASE_URL}/students`;
@@ -67,42 +67,163 @@ const Navbar = () => {
 };
 
 const AddStudentForm = () => {
+  const student_name = van.state("");
+  const student_surname = van.state("");
+  const student_class = van.state("");
   return div(
     { class: "mb-4 inline-block" },
-    input({ type: "text", name: "name", placeholder: "Student First-name", class: "border p-2 mb-2 w-[300px] block" }),
-    input({ type: "text", name: "name", placeholder: "Student Surname", class: "border p-2 mb-2 w-[300px] block" }),
-    input({ type: "text", name: "name", placeholder: "Student Class", class: "border p-2 mb-2 w-[300px] block" }),
-    button({ type: "submit", class: "bg-green-500 text-white px-4 py-2" }, "Add Student")
+    input({
+      type: "text",
+      name: "name",
+      placeholder: "Student First-name",
+      class: "border p-2 mb-2 w-[300px] block",
+      value: student_name,
+      oninput: e => student_name.val = e.target.value
+    }),
+    input({
+      type: "text",
+      name: "name",
+      placeholder: "Student Surname",
+      class: "border p-2 mb-2 w-[300px] block",
+      value: student_surname,
+      oninput: e => student_surname.val = e.target.value
+    }),
+    van.derive(() => select({
+      class: "border p-2 mb-2 w-[300px] block",
+      value: student_class,
+      oninput: e => student_class.val = e.target.value
+    }, [option({ value: "0", selected: true, disabled: true }, "Select class"), ...classesState.val.map(v => option({ value: v.id }, v.name))])),
+    button({
+      type: "submit",
+      class: "bg-green-500 text-white px-4 py-2",
+      onclick: async () => {
+        await postAPI(STUDENTS_URL, {
+          first_name: student_name.val,
+          surname: student_surname.val,
+          class: student_class.val,
+        });
+        await fetchStudents();
+      }
+    }, "Add Student")
   );
 }
 
 const AddClassForm = () => {
+  const class_name = van.state("");
   return div(
     { class: "mb-4 inline-block" },
-    input({ type: "text", name: "name", placeholder: "Class", class: "border p-2 mb-2 w-[300px] block" }),
-    button({ type: "submit", class: "bg-green-500 text-white px-4 py-2" }, "Add Class")
+    input({
+      type: "text",
+      name: "name",
+      placeholder: "Class",
+      class: "border p-2 mb-2 w-[300px] block",
+      value: class_name,
+      oninput: e => class_name.val = e.target.value
+    }),
+    button({
+      type: "submit",
+      class: "bg-green-500 text-white px-4 py-2",
+      onclick: async () => {
+        await postAPI(CLASSES_URL, {
+          name: class_name.val
+        });
+        await fetchClasses();
+      }
+    }, "Add Class")
   );
 }
 
 const AddGradeForm = () => {
+  const student_id = van.state("");
+  const teacher_id = van.state("");
+  const grade = van.state(1);
   return div(
     { class: "mb-4" },
-    input({ type: "text", name: "name", placeholder: "Student Id", class: "border p-2 mb-2 w-[300px] block" }),
-    input({ type: "text", name: "name", placeholder: "Teacher Id", class: "border p-2 mb-2 w-[300px] block" }),
-    input({ type: "text", name: "name", placeholder: "Grade/Score", class: "border p-2 mb-2 w-[300px] block" }),
-    input({ type: "text", name: "name", placeholder: "Year", class: "border p-2 mb-2 w-[300px] block" }),
-    button({ type: "submit", class: "bg-green-500 text-white px-4 py-2" }, "Add Grade")
+    van.derive(() => select({
+      class: "border p-2 mb-2 w-[300px] block",
+      value: student_id,
+      oninput: e => student_id.val = e.target.value
+    }, [option({ value: "0", selected: true, disabled: true }, "Select Student"), ...studentsState.val.map(v => option({ value: v.id }, `${v.first_name} ${v.surname} ${v.class_name}`))])),
+    van.derive(() => select({
+      class: "border p-2 mb-2 w-[300px] block",
+      value: teacher_id,
+      oninput: e => teacher_id.val = e.target.value
+    }, [option({ value: "0", selected: true, disabled: true }, "Select Teacher"), ...teachersState.val.map(v => option({ value: v.id }, `${v.first_name} ${v.surname} ${v.teacher_of}`))])),
+    input({
+      type: "number",
+      min: 1,
+      max: 6,
+      name: "name",
+      placeholder: "Grade/Score",
+      class: "border p-2 mb-2 w-[300px] block",
+      value: grade,
+      oninput: e => grade.val = e.target.value
+    }),
+    button({
+      type: "submit",
+      class: "bg-green-500 text-white px-4 py-2",
+      onclick: async () => {
+        await postAPI(GRADES_URL, {
+          student_id: parseInt(student_id.val),
+          teacher_id: parseInt(teacher_id.val),
+          score: parseInt(grade.val),
+          created_at: "2024",
+        });
+        await fetchGrades();
+      }
+    }, "Add Grade")
   );
 }
 
 const AddTeacherForm = () => {
+  const teacher_name = van.state("");
+  const teacher_surname = van.state("");
+  const teacher_subject = van.state("");
+  const teacher_class = van.state("");
   return div(
     { class: "mb-4" },
-    input({ type: "text", name: "name", placeholder: "Teacher First-name", class: "border p-2 mb-2 w-[400px] block" }),
-    input({ type: "text", name: "name", placeholder: "Teacher Surname", class: "border p-2 mb-2 w-[400px] block" }),
-    input({ type: "text", name: "name", placeholder: "Subject Specialization", class: "border p-2 mb-2 w-[400px] block" }),
-    input({ type: "text", name: "name", placeholder: "Teacher Of Class", class: "border p-2 mb-2 w-[400px] block" }),
-    button({ type: "submit", class: "bg-green-500 text-white px-4 py-2" }, "Add Teacher")
+    input({
+      type: "text",
+      name: "name",
+      placeholder: "Teacher First-name",
+      class: "border p-2 mb-2 w-[400px] block",
+      value: teacher_name,
+      oninput: e => teacher_name.val = e.target.value
+    }),
+    input({
+      type: "text",
+      name: "name",
+      placeholder: "Teacher Surname",
+      class: "border p-2 mb-2 w-[400px] block",
+      value: teacher_surname,
+      oninput: e => teacher_surname.val = e.target.value
+    }),
+    input({
+      type: "text",
+      name: "name",
+      placeholder: "Subject Specialization",
+      class: "border p-2 mb-2 w-[400px] block",
+      value: teacher_subject,
+      oninput: e => teacher_subject.val = e.target.value
+    }),
+    van.derive(() => select({
+      class: "border p-2 mb-2 w-[400px] block",
+      value: teacher_class,
+      oninput: e => teacher_class.val = e.target.value
+    }, [option({ value: "0", selected: true, disabled: true }, "Select class"), ...classesState.val.map(v => option({ value: v.id }, v.name))])),
+    button({
+      type: "submit",
+      class: "bg-green-500 text-white px-4 py-2",
+      onclick: async () => {
+        await postAPI(TEACHERS_URL, {
+          first_name: teacher_name.val,
+          surname: teacher_surname.val,
+          subject: teacher_subject.val,
+          teacher_of: teacher_class.val,
+        });
+        await fetchTeachers();
+      }
+    }, "Add Teacher")
   );
 }
 
